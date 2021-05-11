d6d7466961520a01cff13f6cd92669a33fd0ce2e
ilya <ya.ko4erapbuhunyx@yandex.ru>
2021-05-11 11:33:33 +0300

    add sniff to Frame 11.02.2021

--- sniffer.py
+++ sniffer.py
@@ -1,24 +1,37 @@
+import re
+from datetime import datetime
 import scapy.all as scapy
 
 f = open('text.txt', 'w')
+
+
 class MySniffer():
     def __init__(self):
-
         self.count = 0
 
     def sniff(self, interface):
         scapy.sniff(iface=interface, store=False, prn=self.process_sniffed_packet)
 
     def process_sniffed_packet(self, packet):
-        print(packet)
+        str_packet = str(packet)
+        project, result, time = self.RegularPacketTest_1(str_packet)
+        # print(project, result, time)
         self.count += 1
-        # f.write(self.count and str(packet) and "\n")
-        self.string = str(self.count) + ") " + str(packet) + "\n\n"
-        self.WriteDataToTXT(self.string)
+        # string = str(self.count) + ") " + str(str_packet) + "\n\n"
+        string = str(self.count) + ") " + str(project) + " " + str(result) + str(time) + "\n\n"
+        self.WriteDataToTXT(string)
 
     def WriteDataToTXT(self, text):
         f.write(str(text))
 
+    def RegularPacketTest_1(self, packet):
+        packet = "start MLDR100 0x7F 0x5F 0x7F 0x3F 0x1F end"
+        project = re.findall(r'^\.*start\s(MLDR\w+)', packet)[0]
+        result = re.findall(r'^\.*start\sMLDR\w+\s(.*?)end', packet)[0]
+        time = datetime.now()
+        # print(project, result, time)
+        # print(re.findall(r'^\.*start\sMLDR\w+\s\w+.*', packet))
+        return project, result, time
 
 mySniff = MySniffer()
 for i in range(100):
