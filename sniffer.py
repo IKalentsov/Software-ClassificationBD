23ef136d8067ca96e303261e8ef82a4859f462a3
Ilya_work <ya.ko4erapbuhunyx@yandex.ru>
2021-05-13 12:58:00 +0300

    first good work program with PC to Microcontroller 1986VExx

--- sniffer.py
+++ sniffer.py
@@ -2,40 +2,50 @@ import re
 from datetime import datetime
 import scapy.all as scapy
 
-f = open('text.txt', 'w')
+f = open('snifferActive.txt', 'w')
 
 
 class MySniffer():
     def __init__(self):
         self.count = 0
 
-    def sniff(self, interface):
+    def sniff(self, interface: str) -> None:
         scapy.sniff(iface=interface, store=False, prn=self.process_sniffed_packet)
 
     def process_sniffed_packet(self, packet):
         str_packet = str(packet)
         project, result, time = self.RegularPacketTest_1(str_packet)
-        # print(project, result, time)
-        self.count += 1
-        # string = str(self.count) + ") " + str(str_packet) + "\n\n"
-        string = str(self.count) + ") " + str(project) + " " + str(result) + str(time) + "\n\n"
-        self.WriteDataToTXT(string)
+        if (project != "" and result != ""):
+            # print(project, result, time)
+            self.count += 1
+            # string = str(self.count) + ") " + str(str_packet) + "\n\n"
+            string = str(self.count) + ") " + str(project) + " " + str(result) + str(time) + "\n\n"
+            # print(string)
+            self.WriteDataToTXT(string)
 
-    def WriteDataToTXT(self, text):
+    def WriteDataToTXT(self, text) -> None:
         f.write(str(text))
 
-    def RegularPacketTest_1(self, packet):
-        packet = "start MLDR100 0x7F 0x5F 0x7F 0x3F 0x1F end"
-        project = re.findall(r'^\.*start\s(MLDR\w+)', packet)[0]
-        result = re.findall(r'^\.*start\sMLDR\w+\s(.*?)end', packet)[0]
+    def RegularPacketTest_1(self, packet) -> tuple:
+        # packet = "4234tart MLDR100 0x7F 0x5F 0x7F 0x3F 0x1F end43342"
+        print(packet)
+        project = ""
+        result = ""
+        if re.findall(r'^.*start\s(MLDR\w+)', packet):
+            project = re.findall(r'^.*start\s(MLDR\w+)', packet)[0]
+        # print(type(project))
+        if re.findall(r'^.*start\sMLDR\w+\s(.*?)end', packet):
+            result = re.findall(r'^.*start\sMLDR\w+\s(.*?)end', packet)[0]
         time = datetime.now()
         # print(project, result, time)
-        # print(re.findall(r'^\.*start\sMLDR\w+\s\w+.*', packet))
+        # project = result = time = ""
         return project, result, time
+
 
 mySniff = MySniffer()
 for i in range(100):
     mySniff.sniff("Ethernet")
+    # mySniff.sniff("Беспроводная сеть")
 f.close()
 
 # f = open('text.txt', 'w')
